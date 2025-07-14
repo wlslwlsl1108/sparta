aa5b8bc60383c11dc1f5ef27cb33ecc2bf53fb68
jinhee kim <lsa68807@hanmail.net>
2025-07-14 17:29:32 +0900

    두번째 숫자 입력 시 exit 종료 기능 추가

--- src/test/java/Lv1/Main.java
+++ src/test/java/Lv1/Main.java
@@ -4,18 +4,23 @@ import java.util.Scanner;
 
 public class Main {
 
+    // main 메서드 = 프로그램 실행 시 가장 먼저 호출
     public static void main(String[] args) {
 
+        // Scanner 객체 생성 //
         // 입력 받기 // => Scanner 사용하려면 import 구문 필요
+        // 입력 받을 수 있게 해주는 Scanner 객체 생성
+        // (System.in = 입력 스트림 = 프로그램이 데이터를 받아들이는 길)
         Scanner scanner = new Scanner(System.in);
 
         // 입력 시작 //
-        System.out.println("계산을 시작하겠습니다.");
+        // 입력 안내문구
+        System.out.println("계산을 시작하겠습니다. 종료를 원하시면 'exit'를 입력하세요.");
 
 
-        // while 로 크게 한번 더 묶기
+        // while 로 크게 한번 더 묶기 (계산기 전체 루프)
         while (true) {
-            // while 상단에 변수 정리
+            // while 상단에 변수 선언 및 초기화
             int num1;
             int num2;
             char operator;
@@ -24,6 +29,8 @@ public class Main {
             while (true) {
                 // 첫번 째 숫자 입력받기 (0포함 양의 정수) //
                 System.out.print("첫 번째 숫자(0포함 양의 정수)를 입력하세요: ");
+
+                // input => 한 줄 전체를 문자열로 받는다 (nextLine)
                 String input = scanner.nextLine();
 
                 // exit 입력 시 계산기 정상종료
@@ -34,10 +41,10 @@ public class Main {
                 }
 
                 try {
-                    // exit 종료문 추가하면서 수정 필요 num1 = scanner.nextInt();
+                    // exit 종료문 추가하면서 삭제 => num1 = scanner.nextInt();
                     // 입력 버퍼 비우기
                     // 입력 버퍼 안비우면 연산자로 넘어갔을 때 저장된 숫자로 인해 오류메세지 뜬다.
-                    // exit 종료문 추가하면서 수정 필요 scanner.nextLine();
+                    // exit 종료문 추가하면서 삭제 => scanner.nextLine();
                     num1 = Integer.parseInt(input); //수정완료
 
                     // 음수 입력 시 오류 메시지 후 재입력
@@ -52,18 +59,19 @@ public class Main {
                     // 입력 버퍼 비우기
                     // 입력 버퍼 안비우면 무한루프 생성됨
                     // scanner.nextLine(); =>종료문 추가하면서 삭제
+                    // 예외 발생시에도 nextLine()으로 버퍼를 정리해줘야 했지만 구조가 바뀌면서 필요 없어짐
                 }
 
             }
 
             while (true) {
+
                 // 연산자 입력받기 //
                 System.out.print("연산자( +, -, *, /)를 입력하세요: ");
 
                 // input 정의//
                 // 연산자 길이 체크를 위해 필요 (연산자 반복입력 오류 해결)
                 String input = scanner.nextLine();
-
 
                 // 연산자 길이 체크 //
                 if (input.length() != 1) {
@@ -85,31 +93,42 @@ public class Main {
 
             }
 
-
-            // 두번 째 숫자 입력받기 (0포함 양의 정수) //
             // 음수 입력 시 경고 메세지 후 재입력 가능 //
 
             while (true) {
+                // 두번 째 숫자 입력받기 (0포함 양의 정수) //
+                System.out.print("두 번째 숫자(0포함 양의 정수)를 입력하세요: ");
+
+                //input => 한 줄 전체를 문자열로 받는다. (nextLine)
+                String input = scanner.nextLine();
+
+                //exit 입력 시 계산기 정상 종료
+                if (input.equalsIgnoreCase("exit")){
+                    System.out.println("계산기를 종료합니다.");
+                    System.exit(0);  // 시스템 정상 종료
+                }
 
                 try {
-                    System.out.print("두 번째 숫자(0포함 양의 정수)를 입력하세요: ");
-                    num2 = scanner.nextInt();
 
+                    num2 = Integer.parseInt(input); // num1과 동일하게 추가
+
+                    // 음수 입력 시 오류 메세지 출력 후, 재입력 가능
                     if (num2 < 0) {
                         System.out.println("음수는 입력할 수 없습니다. 0 포함한 숫자(양의 정수)만 입력해주세요.");
                         continue;
                     }
 
-                    // 분모 0 오류 추가
+                    // 분모 0 오류 메세지 출력 후, 재입력 가능
                     if (operator == '/' && num2 == 0) {
                         System.out.println("0으로 나눌 수 없습니다.");
                         continue;
                     }
                     break;
+
                 } catch (Exception e) {
                     System.out.println("문자는 입력할 수 없습니다. 0 포함한 숫자(양의 정수)만 입력해주세요.");
-                    // 입력 버퍼 비우기
-                    scanner.nextLine();
+                    // 입력 버퍼 비우기 삭제 => 위에서 num2 정의를 바꿨으므로
+                    // scanner.nextLine();
                 }
 
             }
