416514dfc800f11c8528aad82a30c375acc12c03
jinhee kim <lsa68807@hanmail.net>
2025-07-14 11:12:18 +0900

    음수입력, 문자입력 예외처리 완료

--- src/test/java/Lv1/Main.java
+++ src/test/java/Lv1/Main.java
@@ -6,48 +6,67 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // 입력 받기 => Scanner 사용하려면 import 구문 필요
+        // 입력 받기 // => Scanner 사용하려면 import 구문 필요
         Scanner scanner = new Scanner(System.in);
 
-        // 입력 시작
+        // 입력 시작 //
         System.out.println("계산을 시작하겠습니다.");
-
 
         int num1;
 
-        // 첫번 째 숫자 입력받기 (0포함 양의 정수)
+        // 첫번 째 숫자 입력받기 (0포함 양의 정수) //
+        // 음수 입력 시 경고 메세지 후 재입력 가능 //
 
         while (true) {
-            System.out.print("첫 번째 숫자(0포함 양의 정수)를 입력하세요: ");
-            num1 = scanner.nextInt();
 
-            if (num1 >= 0){
+            try {
+                System.out.print("첫 번째 숫자(0포함 양의 정수)를 입력하세요: ");
+                num1 = scanner.nextInt();
+
+                if (num1 < 0) {
+                    System.out.println("x! 음수는 입력할 수 없습니다. 0 포함한 숫자(양의 정수)만 입력해주세요.");
+                    continue;
+                }
                 break;
-            } else {
-                System.out.println("잘못입력하였습니다. 0을 포함한 숫자(양의 정수)만 입력하세요.");
+            }
+            catch ( Exception e) {
+                System.out.println("문자는 입력할 수 없습니다. 0 포함한 숫자(양의 정수)만 입력해주세요.");
+                // 입력 버퍼 비우기
+                scanner.nextLine();
             }
 
         }
 
         int num2;
 
-        // 두번 째 숫자 입력받기 (0포함 양의 정수)
+        // 두번 째 숫자 입력받기 (0포함 양의 정수) //
+        // 음수 입력 시 경고 메세지 후 재입력 가능 //
 
         while (true) {
-            System.out.print("두 번째 숫자(0포함 양의 정수)를 입력하세요: ");
-            num2 = scanner.nextInt();
 
-            if (num2 >= 0){
+            try {
+                System.out.print("두 번째 숫자(0포함 양의 정수)를 입력하세요: ");
+                num2 = scanner.nextInt();
+
+                if (num2 < 0) {
+                    System.out.println("x! 음수는 입력할 수 없습니다. 0 포함한 숫자(양의 정수)만 입력해주세요.");
+                    continue;
+                }
                 break;
-            } else {
-                System.out.println("잘못입력하였습니다. 0을 포함한 숫자(양의 정수)만 입력하세요.");
+            }
+            catch ( Exception e) {
+                System.out.println("문자는 입력할 수 없습니다. 0 포함한 숫자(양의 정수)만 입력해주세요.");
+                // 입력 버퍼 비우기
+                scanner.nextLine();
             }
 
         }
 
 
-        // 연산자 입력받기
+        // 연산자 입력받기 //
         System.out.print("연산자( +, -, *, /)를 입력하세요: ");
+
+        // 사칙연산 기호 chaAt(0) 변수에 저장 //
         char operator = scanner.next().charAt(0);
 
         int result = 0;
@@ -65,6 +84,7 @@ public class Main {
                 result = num1 * num2;
                 break;
 
+            // 나눗셈 경우, 분모에 0이 오면 경고 메세지 후, 재입력 가능 //
             case '/':
                 if (num2 == 0){
                     System.out.println("분모에는 0이 올 수 없습니다.");
@@ -73,6 +93,8 @@ public class Main {
                     break;
                 }
         }
+
+        // 계산 결과 출력 //
         System.out.println("결과는: " + result);
     }
 
