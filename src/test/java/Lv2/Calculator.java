fdf2f67455b11b3122739aefdad6fc513c0824f5
jinhee kim <lsa68807@hanmail.net>
2025-07-15 00:25:21 +0900

    연산 결과 저장하는 컬렉션 타입 필드 추가

--- src/test/java/Lv2/Calculator.java
+++ src/test/java/Lv2/Calculator.java
@@ -1,6 +1,12 @@
 package Lv2;
 
+import java.util.ArrayList;
+
+// 연산 결과를 저장하는 컬렉션 타입 필드 가진 Calculator 클래스 생성 //
 public class Calculator {
+
+    // 컬렉션 타입 필드 //
+    public ArrayList<Integer> arrayList = new ArrayList<Integer>();
 
     // 사칙연산 수행 후 연산처리결과값 반환하는 메서드 //
     public int calculate(int num1, int num2, char operator){
