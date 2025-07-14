a1e38184ad464f526160cf497bffc0a5f8d892c0
jinhee kim <lsa68807@hanmail.net>
2025-07-14 16:19:49 +0900

    첫번째 숫자 입력 시 exit 종료 기능 추가

--- src/test/java/Lv1/Main.java
+++ src/test/java/Lv1/Main.java
@@ -22,26 +22,36 @@ public class Main {
             int result = 0;
 
             while (true) {
-
                 // 첫번 째 숫자 입력받기 (0포함 양의 정수) //
-                // 음수 입력 시 경고 메세지 후 재입력 가능 //
+                System.out.print("첫 번째 숫자(0포함 양의 정수)를 입력하세요: ");
+                String input = scanner.nextLine();
+
+                // exit 입력 시 계산기 정상종료
+                if (input.equalsIgnoreCase("exit")){
+                    System.out.println("계산기를 종료합니다.");
+                    //시스템 정상종료
+                    System.exit(0);
+                }
+
                 try {
-                    System.out.print("첫 번째 숫자(0포함 양의 정수)를 입력하세요: ");
-                    num1 = scanner.nextInt();
+                    // exit 종료문 추가하면서 수정 필요 num1 = scanner.nextInt();
                     // 입력 버퍼 비우기
                     // 입력 버퍼 안비우면 연산자로 넘어갔을 때 저장된 숫자로 인해 오류메세지 뜬다.
-                    scanner.nextLine();
+                    // exit 종료문 추가하면서 수정 필요 scanner.nextLine();
+                    num1 = Integer.parseInt(input); //수정완료
 
+                    // 음수 입력 시 오류 메시지 후 재입력
                     if (num1 < 0) {
                         System.out.println("음수는 입력할 수 없습니다. 0 포함한 숫자(양의 정수)만 입력해주세요.");
                         continue;
                     }
                     break;
+
                 } catch (Exception e) {
                     System.out.println("문자는 입력할 수 없습니다. 0 포함한 숫자(양의 정수)만 입력해주세요.");
                     // 입력 버퍼 비우기
                     // 입력 버퍼 안비우면 무한루프 생성됨
-                    scanner.nextLine();
+                    // scanner.nextLine(); =>종료문 추가하면서 삭제
                 }
 
             }
@@ -66,10 +76,10 @@ public class Main {
                 operator = input.charAt(0);
 
                 // while 문 삭제
-                    if (operator != '+' && operator != '-' && operator != '*' && operator != '/') {
-                        System.out.println("x!! 잘못된 연산자입니다. 다시 연산자( +, -, * / )를 입력해주세요.");
-                        continue;
-                    }
+                if (operator != '+' && operator != '-' && operator != '*' && operator != '/') {
+                    System.out.println("x!! 잘못된 연산자입니다. 다시 연산자( +, -, * / )를 입력해주세요.");
+                    continue;
+                }
 
                 break;
 
@@ -120,8 +130,8 @@ public class Main {
 
                 // 나눗셈 경우, 분모에 0이 오면 경고 메세지 후, 재입력 가능 //
                 case '/':
-                        result = num1 / num2;
-                        break;
+                    result = num1 / num2;
+                    break;
 
             }
             // 계산 결과 출력 //
@@ -131,10 +141,3 @@ public class Main {
         }
     }
 }
-
-
-
-
-
-
-
