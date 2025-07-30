f69a5e895a5bb8adc289f01f2c668bd1cf3c48e4
jinhee kim <lsa68803@naver.com>
2025-07-30 20:32:44 +0900

    Add ArrayProcessor and update .gitignore

--- Java-test/src/Q1/ArrayProcessor.java
+++ Java-test/src/Q1/ArrayProcessor.java
@@ -5,12 +5,18 @@ public class ArrayProcessor {
     public int sumEvenNumbers(int[] numbers) {
         // TODO: 이 메서드를 완성하세요.
         // 1. 합계를 저장할 변수를 선언하세요.
+        int sum = 0;
         // 2. 반복문을 사용해 배열의 모든 요소를 확인하세요.
+        for (int number : numbers){
         // 3. 현재 요소가 짝수인지 확인하는 조건문을 작성하세요.
+            if(number % 2 == 0){
         // 4. 짝수라면 합계 변수에 더하세요.
+                sum += number;
+            }
+        }
         // 5. 반복문이 끝나면 최종 합계를 반환하세요.
 
-        return 0; // 임시 반환 값
+        return sum; // 임시 반환 값
     }
 
     public static void main(String[] args) {
