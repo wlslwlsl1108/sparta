859bc073e5af0ae85afea97497d7f878d1cdeea3
jinhee kim <lsa68803@naver.com>
2025-07-31 10:31:24 +0900

    Q4 작성완료

--- Java-test/src/Q4/BaseballManager.java
+++ Java-test/src/Q4/BaseballManager.java
@@ -10,8 +10,8 @@ public class BaseballManager {
         Player player2 = new Player("김혜성");
 
         // 팀에 선수 추가 TODO: Team의 addPlayer 메서드 완성후 아래 주석을 해제해주세요.
-        // myTeam.addPlayer(player1);
-        // myTeam.addPlayer(player2);
+        myTeam.addPlayer(player1);
+        myTeam.addPlayer(player2);
 
         // 팀 선수 목록 출력
         myTeam.printPlayerList(); // 예상 결과: 오타니, 김혜성 출력
--- Java-test/src/Q4/Team.java
+++ Java-test/src/Q4/Team.java
@@ -1,19 +1,29 @@
 package Q4;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 class Team {
     String teamName;
     /* TODO: 올바른 타입의 필드(속성) 선언 변수명은 playerList; */
+    List<Player> playerList;
 
     public Team(String teamName) {
         this.teamName = teamName;
         /* TODO: 위에서 선언한 playerList 필드 초기화 */
+        this.playerList = new ArrayList<Player>();
     }
 
-    public void addPlayer(/* TODO: 올바른 매개변수를 선언하세요 */) {
+    public void addPlayer(Player player/* TODO: 올바른 매개변수를 선언하세요 */) {
         // TODO: 매개변수로 받은 player 객체를 playerList에 추가하세요.
+        this.playerList.add(player);
     }
 
     public void printPlayerList() {
         // TODO: 반복문을 사용하여 playerList에 있는 모든 선수의 이름을 출력하세요.
+        for (Player player : this.playerList) {
+            System.out.println(player.name);
+        }
     }
 }
